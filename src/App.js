c181f8ebece062bab8fc06ffe663a240558d44da
ppaunovski <paunovskipavel@gmail.com>
2022-12-12 13:30:12 +0100

    router, pages, components folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Menupage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Menupage() {
+  return <div>Menupage</div>;
+}
+
+export default Menupage;
