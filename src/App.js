ab4c611d4bdb228299ed9121dd69a64fc5b12aa6
ppaunovski <paunovskipavel@gmail.com>
2022-12-12 16:48:11 +0100

    first menu prototype

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Menupage from "./pages/Menupage";
-import NavigationBar from "./components/NavigationBar"
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route path="/menu" element={<Menupage />} />
+          <Route path="/menu/:whichMenu" element={<Menupage />} />
         </Routes>
       </Router>
     </div>
