849cc4a23a764417c33abeb8b2c4db62812c8ac6
viktorVasilev42 <vasilevviktor03@gmail.com>
2022-12-12 13:40:46 +0100

    Navigation First Test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Menupage from "./pages/Menupage";
+import NavigationBar from "./components/NavigationBar"
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <Router>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
