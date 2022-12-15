a83c8801c1f9ecc57cf63ea737f21c3ea5165010
viktorVasilev42 <vasilevviktor03@gmail.com>
2022-12-15 17:07:16 +0100

    Map First

--- src/pages/FindUs.js
+++ src/pages/FindUs.js
@@ -4,6 +4,9 @@ import "../styles/FindUs.css";
 export default function FindUs(props) {
     const [isPanning, setPanning] = useState(false);
     const [oldX, setOldX] = useState(0);
+    const [oldY, setOldY] = useState(0);
+    const [leftCount, setLeftCount] = useState(0);
+    const [topCount, setTopCount] = useState(0);
 
     useEffect(() => {
         if (isPanning) {
@@ -18,25 +21,57 @@ export default function FindUs(props) {
         <div>
             <input type="checkbox" checked={props.aboutShow} id="my-modal" className="modal-toggle" />
             <div className="modal">
-                <div className="modal-box relative">
+                <div className="modal-box relative" style={{height: 900, maxWidth: 1000, width: 900,}}>
                     <h1 className="absolute right-8 top-3 cursor-pointer" onClick={() => props.setAboutShow(false)}>X</h1>
                     <h1>Map</h1>
-                    <div className="overflow-hidden h-64">
-                        <img id="mapImg" className="unselectable object-cover relative" style={{ height: "300%", width: "300%" }} src="https://ontheworldmap.com/macedonia/city/skopje/detailed-map-of-skopje.jpg"
+                    <div className="overflow-hidden relative w-full" style={{height: "95%"}}>
+                        <img id="mapImg" className='MapImg unselectable absolute cursor-move' style={{ transform: "scale(2)"}} src="https://cdn.discordapp.com/attachments/385909243984740352/1052945337876230164/image.png"
                             onMouseDown={(e) => {
                                 setPanning(true);
                                 setOldX(e.clientX);
+                                setOldY(e.clientY);
                             }}
                             onMouseUp={(e) => {
                                 setPanning(false);
                                 setOldX(e.clientX);
+                                setOldY(e.clientY);
                             }} 
                             onMouseMove={(e) => {
                                 if (isPanning) {
+                                    setOldX(e.clientX);
+                                    setOldY(e.clientY);
                                     if (e.clientX > oldX) {
-                                        document.getElementById("mapImg").style.left += 1;
+                                        if (leftCount > 400) {
+                                            return;
+                                        }
+                                        
+                                        setLeftCount(leftCount+9);
+                                        document.querySelector(".MapImg").style.left = leftCount + "px";
+                                    }
+                                    if (e.clientX < oldX) {
+                                        if (leftCount > document.querySelector(".MapImg").clientWidth) {
+                                            return;
+                                        }
+                                        
+                                        setLeftCount(leftCount-9);
+                                        document.querySelector(".MapImg").style.left = leftCount + "px";
+                                    }
+                                    if (e.clientY > oldY) {
+                                        if (topCount > 0) {
+                                            return;
+                                        }
+
+                                        setTopCount(topCount+9);
+                                        document.querySelector(".MapImg").style.top = topCount + "px";
+                                    }
+                                    if (e.clientY < oldY) {
+                                        setTopCount(topCount-9);
+                                        document.querySelector(".MapImg").style.top = topCount + "px";
                                     }
                                 }
+                            }}
+                            onMouseLeave={() => {
+                                setPanning(false);
                             }} />
                     </div>
                 </div>
