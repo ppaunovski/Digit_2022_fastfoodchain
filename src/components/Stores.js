4fbc659a2c5fd1724f6a0e2017387126078e32b9
ppaunovski <paunovskipavel@gmail.com>
2022-12-16 12:51:18 +0100

    stores

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -36,7 +36,7 @@ function Stores() {
           className="carousel-item flex items-center justify-center relative w-full"
         >
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/digit-2022.appspot.com/o/Sugar%20Box%20Cake%20Shop%20in%20Saudi%20Arabia.jpg?alt=media&token=eea77dc7-1292-42d1-8acd-f85f6c442033"
+            src="https://firebasestorage.googleapis.com/v0/b/digit-2022.appspot.com/o/store.jpeg?alt=media&token=576a7a38-4b22-4707-9029-c9f43f96d3a9"
             className="w-[80vw] lg:w-[65vw] xl:w-[50vw]"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -53,7 +53,7 @@ function Stores() {
           className="carousel-item flex items-center justify-center relative w-full"
         >
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/digit-2022.appspot.com/o/Sugar%20Box%20Cake%20Shop%20in%20Saudi%20Arabia.jpg?alt=media&token=eea77dc7-1292-42d1-8acd-f85f6c442033"
+            src="https://firebasestorage.googleapis.com/v0/b/digit-2022.appspot.com/o/inside.jpeg?alt=media&token=9710aed1-e612-4bf5-82bc-bf821f2cba75"
             className="w-[80vw] lg:w-[65vw] xl:w-[50vw]"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -70,7 +70,7 @@ function Stores() {
           className="carousel-item flex items-center justify-center relative w-full"
         >
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/digit-2022.appspot.com/o/Sugar%20Box%20Cake%20Shop%20in%20Saudi%20Arabia.jpg?alt=media&token=eea77dc7-1292-42d1-8acd-f85f6c442033"
+            src="https://firebasestorage.googleapis.com/v0/b/digit-2022.appspot.com/o/interior-cafe.jpg?alt=media&token=6426127e-5ed0-429f-8a19-fa5ea2894ede"
             className="w-[80vw] lg:w-[65vw] xl:w-[50vw]"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
